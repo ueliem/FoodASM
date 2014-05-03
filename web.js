49a81b336afcacc103ae430e282fe5ff8c757645
Will Long <will.long043@gmail.com>
2014-05-03 17:53:06 -0400

    Added Jade files, js, and css.

--- web.js
+++ web.js
@@ -1,8 +1,14 @@
 var express = require('express');
 var app = express();
 
+app.set('views', __dirname + '/views')
+app.set('view engine', 'jade')
+app.use(express.static(__dirname + '/public'))
+
 app.get('/', function(req, res){
-  res.send('Hello World');
+  res.render('index',
+  { title : 'Home' }
+  )
 });
 
 app.listen(3000);
