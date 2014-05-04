0afc7d9263054553c69d3b0d78ed44359c45ca94
Will Long <will.long043@gmail.com>
2014-05-03 22:49:02 -0400

    Added API call to pearson recipes.

--- public/js/controllers.js
+++ public/js/controllers.js
@@ -2,7 +2,21 @@ var fasmControllers = angular.module('fasmControllers', []);
 
 fasmControllers.controller('SearchController', ['$scope', '$http', 'searchResultsService',
     function($scope, $http, searchResultsService) {
+        $scope.cur_search = "";
+        $scope.get_recipes_by_ingredients = function(cur_search) {
+            var requrl = "https://api.pearson.com/v2/foodanddrink/recipes?search=" + cur_search;
+            $http.get(requrl)
+            .success(function(data) {
+                console.log("Something not as bad happened.");
+                var numrecipes = data.count;
+                var recipes = data.results;
+            }).error(function(data) {
+                console.log("Something horrible happened.");
+            });
 
-        $scope.get_recipes_by_ingredients = function(item) { searchResultsService.get_recipes_by_ingredients(item); };
+
+
+            //searchResultsService.get_recipes_by_ingredients(cur_search); console.log("SEARCHED");
+        };
     }
 ]);
--- public/js/services.js
+++ public/js/services.js
@@ -6,12 +6,5 @@ fasmServices.service('searchResultsService', function() {
     var numrecipes = 0;
 
     this.get_recipes_by_ingredients = function(ingredientname) {
-        $http.jsonp("https://api.pearson.com/v2/foodanddrink/recipes?search=" + ingredientname)
-        .success(function(data) {
-            numrecipes = data.count;
-            recipes = data.results;
-        }).error(function(data) {
-            console.log("Something horrible happened.");
-        });
     };
 });
