435d0e31dd0cff7dc946f215bd93f85019289d8a
Will Long <will.long043@gmail.com>
2014-05-03 21:55:20 -0400

    Added search controller and service.

--- public/js/controllers.js
+++ public/js/controllers.js
@@ -1,5 +1,8 @@
 var fasmControllers = angular.module('fasmControllers', []);
 
-fasmControllers.controller('SearchController', ['$scope',
-  function($scope) {
-  }]);
+fasmControllers.controller('SearchController', ['$scope', '$http', 'searchResultsService',
+    function($scope, $http, searchResultsService) {
+
+        $scope.get_recipes_by_ingredients = function(item) { searchResultsService.get_recipes_by_ingredients(item) };
+    }
+]);
